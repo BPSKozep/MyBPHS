a7ffa4e9259c56a63d7dd9179deaf835285af175
JamDon2 <hello@jamdon2.dev>
2023-06-21 13:43:25 +0200

    main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,35 @@
 export default function Home() {
     return (
         <>
-            <div className="flex bg-slate-800 items-center justify-center h-10">
-                <h1 className="text-center font-bold text-white">
-                    Üdvözlünk a MyBPHS-ben!
+            <div className="flex bg-slate-800 items-center justify-center h-[7vh]">
+                <h1 className="text-center text-xl sm:text-2xl font-bold text-white">
+                    Üdvözlünk a{" "}
+                    <span className="font-handwriting text-amber-400">My</span>
+                    <span className="font-black">BPHS</span>-ben!
                 </h1>
             </div>
 
-            <div className="flex justify-center items-center">
-                <div className="inline-grid gap-4 grid-cols-2 grid-rows-2 text-white m-3">
-                    <div className="border-2 flex bg-slate-800 p-3 rounded-md">
-                        abbbbb
+            <div className="flex justify-center items-center absolute h-[93vh] w-full">
+                <div className="inline-grid gap-4 grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 text-white m-3">
+                    <div className="border flex bg-slate-800 p-5 rounded-md">
+                        <span className="text-center w-full text-xl font-bold">
+                            Kreditek
+                        </span>
                     </div>
-                    <div className="border-2 flex bg-slate-800 p-3 rounded-md">
-                        b
+                    <div className="border flex bg-slate-800 p-5 rounded-md">
+                        <span className="text-center w-full text-xl font-bold">
+                            Hiányzások
+                        </span>
                     </div>
-                    <div className="border-2 flex bg-slate-800 p-3 rounded-md">
-                        c
+                    <div className="border flex bg-slate-800 p-5 rounded-md">
+                        <span className="text-center w-full text-xl font-bold">
+                            Ebédrendelés
+                        </span>
                     </div>
-                    <div className="border-2 flex bg-slate-800 p-3 rounded-md">
-                        d
+                    <div className="border flex bg-slate-800 p-5 rounded-md">
+                        <span className="text-center w-full text-xl font-bold">
+                            Kreditek
+                        </span>
                     </div>
                 </div>
             </div>
