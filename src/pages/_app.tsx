c3912cae670318db35648fc26d14f167163fdcbc
JamDon2 <hello@jamdon2.dev>
2023-06-22 21:25:06 +0200

    add trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,10 @@ import "@fontsource-variable/roboto-slab";
 import "@fontsource/lily-script-one";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { trpc } from "../utils/trpc";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
+
+export default trpc.withTRPC(App);
