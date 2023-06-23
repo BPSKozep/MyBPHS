110b2f73df4f3131b2d5610c3cc416282eff49b0
JamDon2 <hello@jamdon2.dev>
2023-06-23 21:27:48 +0200

    set baseUrl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,10 @@ import "@fontsource-variable/montserrat";
 import "@fontsource-variable/source-code-pro";
 import "@fontsource-variable/roboto-slab";
 import "@fontsource/lily-script-one";
-import "../styles/globals.css";
+import "styles/globals.css";
 import type { AppProps } from "next/app";
-import { trpc } from "../utils/trpc";
-import { wrapper } from "../store/store";
+import { trpc } from "utils/trpc";
+import { wrapper } from "store/store";
 import { Provider } from "react-redux";
 
 function App({ Component, ...rest }: AppProps) {
