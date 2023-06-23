ee150f570abe28ce0c8fa1f87deb9e38caa7ca2c
JamDon2 <hello@jamdon2.dev>
2023-06-23 16:42:41 +0200

    home button

--- src/pages/credits.tsx
+++ src/pages/credits.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 function Credits() {
     return (
         <>
             <div className="flex bg-slate-800 items-center justify-center h-[7vh]">
-                <div className="flex items-center gap-2 cursor-default text-white">
-                    <FontAwesomeIcon icon={faHome} />
+                <div className="flex items-center cursor-default text-white">
+                    <Link href="/" className="absolute left-7">
+                        <FontAwesomeIcon icon={faHome} />
+                    </Link>
                     <h1 className="text-center text-xl sm:text-2xl font-black text-white">
                         Kreditek
                     </h1>
