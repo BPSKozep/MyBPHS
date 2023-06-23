ae82726baa0cea36ee377c1eaac4a56183023d73
JamDon2 <hello@jamdon2.dev>
2023-06-23 21:12:59 +0200

    rename function to mongooseconnect

--- src/clients/mongoose.ts
+++ src/clients/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export default async function connect() {
+export default async function mongooseConnect() {
     return await mongoose.connect(process.env.MONGODB_URI as string, {
         dbName: process.env.MONGODB_DATABASE,
     });
