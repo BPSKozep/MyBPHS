22c03d1f0ab5f1edfb01bbeab3411c8286ae201e
JamDon2 <hello@jamdon2.dev>
2023-06-23 13:21:41 +0200

    use link component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Home() {
-    const router = useRouter();
-
     return (
         <>
             <div className="flex bg-slate-800 items-center justify-center h-[7vh]">
@@ -15,16 +13,13 @@ export default function Home() {
 
             <div className="flex justify-center items-center absolute h-[93vh] w-full">
                 <div className="inline-grid gap-4 grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 text-white m-3">
-                    <div className="border flex bg-slate-800 p-7 rounded-md cursor-pointer hover:bg-slate-700 hover:scale-105 transition-all delay-50 duration-200">
-                        <span
-                            className="text-center w-full text-xl font-bold"
-                            onClick={() => {
-                                router.push("/credits");
-                            }}
-                        >
-                            Kreditek
-                        </span>
-                    </div>
+                    <Link href="/credits">
+                        <div className="border flex bg-slate-800 p-7 rounded-md cursor-pointer hover:bg-slate-700 hover:scale-105 transition-all delay-50 duration-200">
+                            <span className="text-center w-full text-xl font-bold">
+                                Kreditek
+                            </span>
+                        </div>
+                    </Link>
                     <div className="border flex bg-slate-800 p-7 rounded-md cursor-pointer hover:bg-slate-700 hover:scale-105 transition-all delay-50 duration-200">
                         <span className="text-center w-full text-xl font-bold">
                             Hiányzások
