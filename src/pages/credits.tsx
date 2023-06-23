1edbe0b8ac4aeee8a33e65dd3ab46bc2a719cb2d
JamDon2 <hello@jamdon2.dev>
2023-06-23 13:37:49 +0200

    move to fontawesome

--- src/pages/credits.tsx
+++ src/pages/credits.tsx
@@ -1,24 +1,17 @@
 import React from "react";
-import { useRouter } from "next/router";
-import HomeIcon from "@mui/icons-material/Home";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 function Credits() {
-    const router = useRouter();
-
     return (
         <>
-            <div className="flex bg-slate-800 items-center justify-center h-[7vh] gap-4">
-                <HomeIcon
-                    color="primary"
-                    fontSize="large"
-                    onClick={() => {
-                        router.push("/");
-                    }}
-                    className="cursor-pointer"
-                />
-                <h1 className="text-center text-xl sm:text-2xl font-black text-white">
-                    Kreditek
-                </h1>
+            <div className="flex bg-slate-800 items-center justify-center h-[7vh]">
+                <div className="flex items-center gap-2 cursor-default text-white">
+                    <FontAwesomeIcon icon={faHome} />
+                    <h1 className="text-center text-xl sm:text-2xl font-black text-white">
+                        Kreditek
+                    </h1>
+                </div>
             </div>
         </>
     );
