5fcec23d21e6b928b9d1903047ab43531fe5beeb
JamDon2 <hello@jamdon2.dev>
2023-06-24 21:11:49 +0200

    make the buttons disabled
    Co-authored-by: IoMARKoI <markgyoni@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,17 +20,17 @@ export default function Home() {
                             </span>
                         </div>
                     </Link>
-                    <div className="border flex bg-slate-800 p-7 rounded-md cursor-pointer hover:bg-slate-700 hover:scale-105 transition-all delay-50 duration-200">
+                    <div className="border flex bg-slate-600 p-7 rounded-md cursor-not-allowed hover:bg-slate-700 transition-all delay-50 duration-200">
                         <span className="text-center w-full text-xl font-bold">
                             Hiányzások
                         </span>
                     </div>
-                    <div className="border flex bg-slate-800 p-7 rounded-md cursor-pointer hover:bg-slate-700 hover:scale-105 transition-all delay-50 duration-200">
+                    <div className="border flex bg-slate-600 p-7 rounded-md cursor-not-allowed hover:bg-slate-700 transition-all delay-50 duration-200">
                         <span className="text-center w-full text-xl font-bold">
                             Órarend
                         </span>
                     </div>
-                    <div className="border flex bg-slate-800 p-7 rounded-md cursor-pointer hover:bg-slate-700 hover:scale-105 transition-all delay-50 duration-200">
+                    <div className="border flex bg-slate-600 p-7 rounded-md cursor-not-allowed hover:bg-slate-700 transition-all delay-50 duration-200">
                         <span className="text-center w-full text-xl font-bold">
                             Ebédrendelés
                         </span>
