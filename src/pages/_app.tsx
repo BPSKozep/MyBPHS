2797cb4aacb0e71178d9177d9793fa542a8873c0
JamDon2 <hello@jamdon2.dev>
2023-08-22 23:37:11 +0200

    make the page container absolute

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,7 +31,7 @@ function App({ Component, ...rest }: AppProps) {
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.3 }}
-                            className="relative bottom-0 left-0 right-0 top-0"
+                            className="absolute bottom-0 left-0 right-0 top-0"
                         >
                             <Component {...pageProps} />
                         </motion.div>
