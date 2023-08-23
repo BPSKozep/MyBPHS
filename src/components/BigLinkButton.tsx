303b2d2201378e54de655c169eb522b5ecbd0a41
JamDon2 <hello@jamdon2.dev>
2023-08-23 19:16:34 +0200

    move the buttons to a component

---
+++ src/components/BigLinkButton.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+
+function BigLinkButton({
+    title,
+    url,
+    disabled,
+}: {
+    title: string;
+    url: string;
+    disabled?: boolean;
+}) {
+    const router = useRouter();
+
+    if (disabled) {
+        return (
+            <motion.button
+                whileHover={{ rotateZ: [null, 0.5, -0.5, 0] }}
+                transition={{
+                    duration: 0.5,
+                    times: [0, 0.33, 0.66, 1],
+                }}
+                className="flex cursor-not-allowed rounded-md border border-gray-400 bg-slate-700 p-7 text-gray-300"
+                disabled
+            >
+                <span className="w-full text-center text-xl font-bold">
+                    {title}
+                </span>
+            </motion.button>
+        );
+    }
+
+    return (
+        <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileFocus={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{
+                type: "spring",
+                stiffness: 500,
+                damping: 20,
+            }}
+            onClick={() => setTimeout(() => router.push(url), 350)}
+            className="flex cursor-pointer rounded-md border bg-slate-700 p-7"
+        >
+            <span className="w-full text-center text-xl font-bold">
+                {title}
+            </span>
+        </motion.button>
+    );
+}
+
+export default BigLinkButton;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,14 @@
-import { motion } from "framer-motion";
-import { useRouter } from "next/router";
+import BigLinkButton from "components/BigLinkButton";
 
 export default function Home() {
-    const router = useRouter();
-
     return (
         <>
             <div className="flex h-full w-full items-center justify-center">
                 <div className="m-3 inline-grid grid-cols-1 grid-rows-4 gap-4 text-white sm:grid-cols-2 sm:grid-rows-2">
-                    <motion.button
-                        whileHover={{ scale: 1.1 }}
-                        whileFocus={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.95 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 500,
-                            damping: 20,
-                        }}
-                        onClick={() =>
-                            setTimeout(() => router.push("/credits"), 350)
-                        }
-                        className="flex cursor-pointer rounded-md border bg-slate-700 p-7"
-                    >
-                        <span className="w-full text-center text-xl font-bold">
-                            Kreditek
-                        </span>
-                    </motion.button>
-                    <motion.button
-                        whileHover={{ rotateZ: [null, 0.5, -0.5, 0] }}
-                        transition={{
-                            duration: 0.5,
-                            times: [0, 0.33, 0.66, 1],
-                        }}
-                        className="flex cursor-not-allowed rounded-md border border-gray-400 bg-slate-700 p-7 text-gray-300"
-                        disabled
-                    >
-                        <span className="w-full text-center text-xl font-bold">
-                            Hiányzások
-                        </span>
-                    </motion.button>
-                    <motion.button
-                        whileHover={{ rotateZ: [null, 0.5, -0.5, 0] }}
-                        transition={{
-                            duration: 0.5,
-                            times: [0, 0.33, 0.66, 1],
-                        }}
-                        className="flex cursor-not-allowed rounded-md border border-gray-400 bg-slate-700 p-7 text-gray-300"
-                        disabled
-                    >
-                        <span className="w-full text-center text-xl font-bold">
-                            Órarend
-                        </span>
-                    </motion.button>
-                    <motion.button
-                        whileHover={{ rotateZ: [null, 0.5, -0.5, 0] }}
-                        transition={{
-                            duration: 0.5,
-                            times: [0, 0.33, 0.66, 1],
-                        }}
-                        className="flex cursor-not-allowed rounded-md border border-gray-400 bg-slate-700 p-7 text-gray-300"
-                        disabled
-                    >
-                        <span className="w-full text-center text-xl font-bold">
-                            Ebédrendelés
-                        </span>
-                    </motion.button>
+                    <BigLinkButton title="Kreditek" url="/credits" />
+                    <BigLinkButton title="Hiányzások" url="/" disabled />
+                    <BigLinkButton title="Órarend" url="/timetable" />
+                    <BigLinkButton title="Ebédrendelés" url="/" disabled />
                 </div>
             </div>
         </>
