b08da60f8c75169f04281a173c9b580f85a5da82
JamDon2 <hello@jamdon2.dev>
2023-08-27 15:27:12 +0200

    remove flex from pagewithheader

--- src/components/PageWithHeader.tsx
+++ src/components/PageWithHeader.tsx
@@ -8,9 +8,7 @@ function PageWithHeader({
     return (
         <div className="flex h-full w-full flex-col items-center justify-center">
             <PageHeader title={title} />
-            <div className="flex h-full w-full items-center justify-center">
-                {children}
-            </div>
+            <div className="h-full w-full">{children}</div>
         </div>
     );
 }
