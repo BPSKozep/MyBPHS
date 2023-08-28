5cf804c6687191d2b24841681a2bb65d5b7908d5
JamDon2 <hello@jamdon2.dev>
2023-08-28 19:31:20 +0200

    create role checker function

---
+++ src/utils/authorization.ts
@@ -0,0 +1,10 @@
+import { User } from "models";
+import { Session } from "next-auth";
+
+export async function checkRoles(session: Session, allowedRoles: string[]) {
+    const requester = await User.findOne({
+        email: session.user?.email,
+    });
+
+    return allowedRoles.some((role) => requester?.roles.indexOf(role) !== -1);
+}
