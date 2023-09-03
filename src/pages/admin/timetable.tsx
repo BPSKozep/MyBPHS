31a8de42e94244d01eba9237c319522f59d61c13
JamDon2 <hello@jamdon2.dev>
2023-09-03 20:15:33 +0200

    move savebutton to its own component

--- src/pages/admin/timetable.tsx
+++ src/pages/admin/timetable.tsx
@@ -1,12 +1,10 @@
-import { faCheck, faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PageWithHeader from "components/PageWithHeader";
 import TimetableEditor from "components/TimetableEditor";
 import React, { useCallback, useEffect, useState } from "react";
 import { trpc } from "utils/trpc";
-import { motion } from "framer-motion";
 import { useDebounce } from "use-debounce";
 import OnlyRoles from "components/OnlyRoles";
+import SaveButton from "components/SaveButton";
 
 const EMPTY_TIMETABLE = Array(5).fill([]);
 
@@ -16,7 +14,6 @@ function Timetable() {
     const [priority, setPriority] = useState("");
     const [timetable, setTimetable] =
         useState<(string | null)[][]>(EMPTY_TIMETABLE);
-    const [saveAnimation, setSaveAnimation] = useState(false);
 
     const { data } = trpc.group.get.useQuery(debouncedGroupName);
     const { mutate } = trpc.group.update.useMutation();
@@ -60,36 +57,8 @@ function Timetable() {
                             }}
                             placeholder="Prioritás"
                         />
-                        <motion.button
-                            className="h-12 w-12 rounded-2xl p-3 text-white"
-                            initial={{
-                                scale: 1,
-                                backgroundColor: "#565e85",
-                            }}
-                            animate={
-                                saveAnimation
-                                    ? {
-                                          scale: 1.2,
-                                          backgroundColor: "#4abd63",
-                                      }
-                                    : { scale: 1 }
-                            }
-                            transition={{
-                                scale: {
-                                    type: "spring",
-                                    damping: 9,
-                                    stiffness: 200,
-                                },
-                                backgroundColor: {
-                                    type: "tween",
-                                },
-                            }}
-                            whileHover={{
-                                backgroundColor: saveAnimation
-                                    ? "#4abd63"
-                                    : "#3a445d",
-                            }}
-                            onClick={() => {
+                        <SaveButton
+                            onClick={() =>
                                 mutate({
                                     name: groupName,
                                     newValue: {
@@ -97,41 +66,9 @@ function Timetable() {
                                         timetable,
                                         priority: Number(priority),
                                     },
-                                });
-
-                                setSaveAnimation(true);
-
-                                setTimeout(() => setSaveAnimation(false), 800);
-                            }}
-                        >
-                            <div className="relative">
-                                <motion.div
-                                    initial={{ display: "block", opacity: 1 }}
-                                    animate={{
-                                        display: saveAnimation
-                                            ? "none"
-                                            : "block",
-                                        opacity: saveAnimation ? 0 : 1,
-                                    }}
-                                >
-                                    <FontAwesomeIcon
-                                        icon={faFloppyDisk}
-                                        size="xl"
-                                    />
-                                </motion.div>
-                                <motion.div
-                                    initial={{ display: "none", scale: 0 }}
-                                    animate={{
-                                        display: saveAnimation
-                                            ? "block"
-                                            : "none",
-                                        scale: saveAnimation ? 1 : 0,
-                                    }}
-                                >
-                                    <FontAwesomeIcon icon={faCheck} size="xl" />
-                                </motion.div>
-                            </div>
-                        </motion.button>
+                                })
+                            }
+                        />
                     </div>
                     <TimetableEditor
                         timetable={timetable}
