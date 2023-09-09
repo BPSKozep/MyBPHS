78a9fc8f37b40b5b71b6776d39c824daa42a97a7
JamDon2 <hello@jamdon2.dev>
2023-09-09 21:30:21 +0200

    add tabs component

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+function Tabs({
+    options,
+    defaultOption,
+    onChange,
+}: {
+    options: { [key: string]: string };
+    defaultOption: string;
+    onChange: (selected: string) => void;
+}) {
+    const [selectedTab, setSelectedTab] = useState(defaultOption);
+
+    const handleTabChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedTab(event.target.value);
+    };
+
+    useEffect(() => {
+        onChange(selectedTab);
+    }, [selectedTab, onChange]);
+
+    return (
+        <div className="select-none text-white">
+            {Object.entries(options).map(([key, value], index) => (
+                <div key={key} className="inline-block">
+                    <input
+                        type="radio"
+                        id={key}
+                        value={key}
+                        checked={selectedTab === key}
+                        onChange={handleTabChange}
+                        className="hidden"
+                    />
+                    <label
+                        htmlFor={key}
+                        className={`box-border inline-block p-3 text-sm font-bold sm:text-base ${
+                            selectedTab === key
+                                ? "bg-[#434a68]"
+                                : "bg-[#565e85]"
+                        } ${index === 0 ? "rounded-l-md" : ""} ${
+                            index === Object.keys(options).length - 1
+                                ? "rounded-r-md"
+                                : ""
+                        }`}
+                    >
+                        {value}
+                    </label>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Tabs;
