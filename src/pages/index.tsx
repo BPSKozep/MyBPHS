611cdd4887daae0a279640de43df08f3f5d6809e
JamDon2 <hello@jamdon2.dev>
2023-09-09 21:31:03 +0200

    reorganize buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ export default function Home() {
         <>
             <div className="flex h-full w-full items-center justify-center">
                 <div className="m-3 inline-grid grid-cols-1 grid-rows-4 gap-4 text-white sm:grid-cols-2 sm:grid-rows-2">
-                    <BigLinkButton title="Kreditek" url="/credits" />
-                    <BigLinkButton title="Hiányzások" url="/" disabled />
+                    <BigLinkButton title="Kreditek" url="/credits" disabled />
                     <BigLinkButton title="Órarend" url="/timetable" />
                     <BigLinkButton title="Ebédrendelés" url="/" disabled />
+                    <BigLinkButton title="Coming Soon™" url="/" disabled />
                 </div>
             </div>
         </>
