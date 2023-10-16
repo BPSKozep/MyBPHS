53a8771256928d9f7699f09423b9608594e74520
JamDon2 <hello@jamdon2.dev>
2023-10-16 09:18:25 +0200

    add title to pagewithheader

--- src/components/PageWithHeader.tsx
+++ src/components/PageWithHeader.tsx
@@ -1,15 +1,21 @@
 import React, { PropsWithChildren } from "react";
 import PageHeader from "./PageHeader";
+import Head from "next/head";
 
 function PageWithHeader({
     title,
     children,
 }: { title: string } & PropsWithChildren) {
     return (
-        <div className="flex h-full w-full flex-col items-center justify-center">
-            <PageHeader title={title} />
-            <div className="h-full w-full">{children}</div>
-        </div>
+        <>
+            <Head>
+                <title>MyBPHS - {title}</title>
+            </Head>
+            <div className="flex h-full w-full flex-col items-center justify-center overflow-hidden">
+                <PageHeader title={title} />
+                <div className="h-full w-full">{children}</div>
+            </div>
+        </>
     );
 }
 
