2478d2bf74804c7aee31f328bd961d9171271be7
JamDon2 <hello@jamdon2.dev>
2023-10-20 13:04:06 +0200

    disable all buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import BigLinkButton from "components/BigLinkButton";
+import breakpoints from "utils/breakpoints";
 
 export default function Home() {
+    console.log(breakpoints);
+
     return (
         <>
             <div className="flex h-full w-full items-center justify-center">
                 <div className="m-3 inline-grid grid-cols-1 grid-rows-4 gap-4 text-white sm:grid-cols-2 sm:grid-rows-2">
                     <BigLinkButton title="Kreditek" url="/credits" disabled />
-                    <BigLinkButton title="Órarend" url="/timetable" />
-                    <BigLinkButton title="Ebédrendelés" url="/" disabled />
+                    <BigLinkButton title="Órarend" url="/timetable" disabled />
+                    <BigLinkButton title="Ebédrendelés" url="/lunch" disabled />
                     <BigLinkButton title="Coming Soon™" url="/" disabled />
                 </div>
             </div>
