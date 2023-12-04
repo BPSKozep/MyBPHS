af9fc0386a2664ee0ebff2ff495804ab7467b80f
JamDon2 <hello@jamdon2.dev>
2023-12-04 13:02:47 +0100

    add disabled option to transpose

--- src/utils/transpose.ts
+++ src/utils/transpose.ts
@@ -1,5 +1,8 @@
 export default function transpose2DArray<ArrayType>(
-    array: ArrayType[][]
+    array: ArrayType[][],
+    disabled: boolean
 ): ArrayType[][] {
+    if (disabled) return array;
+
     return array[0].map((_, index) => array.map((row) => row[index]));
 }
