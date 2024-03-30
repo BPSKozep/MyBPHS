8208749ea203f9699ffe1e3a9418310316dd7419
markgyoni <markgyoni@gmail.com>
2024-03-30 19:44:56 +0100

    Add basic 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function notFound() {
+    return (
+        <div className="flex h-full items-center justify-center">
+            <h1 className="text-xl text-white">Az oldal nem található.</h1>
+        </div>
+    );
+}
