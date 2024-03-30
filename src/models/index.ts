44a70eaefe730971709f1b83f5e035dab1139a4b
JamDon2 <hello@jamdon2.dev>
2024-03-30 12:40:09 +0100

    add new models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,7 @@
 import User from "./User.model";
 import Group from "./Group.model";
 import GroupOverride from "./GroupOverride.model";
+import Menu from "./Menu.model";
+import Order from "./Order.model";
 
-export { User, Group, GroupOverride };
+export { User, Group, GroupOverride, Menu, Order };
