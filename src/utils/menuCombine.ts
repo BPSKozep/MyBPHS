9592d4382a85d1b1de2ae19d21a4ffc208eb3d1b
JamDon2 <hello@jamdon2.dev>
2024-03-30 23:57:33 +0100

    allow excluding no order

--- src/utils/menuCombine.ts
+++ src/utils/menuCombine.ts
@@ -2,6 +2,15 @@ import globalOptions from "data/global_options.json";
 
 type Menu = Record<string, string>;
 
-export default function menuCombine(menu: Menu): Menu {
-    return { ...globalOptions, ...menu };
+export default function menuCombine(
+    menu: Menu,
+    include_no_order: boolean = true
+): Menu {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { i_am_not_want_food: _, ...globalOptionsOmitted } = globalOptions;
+
+    return {
+        ...menu,
+        ...(include_no_order ? globalOptions : globalOptionsOmitted),
+    };
 }
