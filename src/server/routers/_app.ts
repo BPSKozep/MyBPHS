f89bc426f3c4d3cefe2e1ce83f8c89f488d33c1a
JamDon2 <hello@jamdon2.dev>
2024-03-31 22:14:07 +0200

    add email router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -4,12 +4,14 @@ import userRouter from "./user.router";
 import groupRouter from "./group.router";
 import menuRouter from "./menu.router";
 import orderRouter from "./order.router";
+import emailRouter from "./email.router";
 
 export const appRouter = router({
     user: userRouter,
     group: groupRouter,
     menu: menuRouter,
     order: orderRouter,
+    email: emailRouter,
 });
 
 // export type definition of API
---
+++ src/server/routers/email.router.ts
@@ -0,0 +1,39 @@
+import { Resend } from "resend";
+import { procedure, router } from "../trpc";
+import Lunch from "emails/lunch";
+import { checkRoles } from "utils/authorization";
+import { TRPCError } from "@trpc/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const emailRouter = router({
+    sendLunchEmail: procedure.mutation(async ({ ctx }) => {
+        if (!ctx.session) {
+            throw new TRPCError({
+                code: "UNAUTHORIZED",
+                message: "Unauthorized",
+            });
+        }
+
+        const authorized = await checkRoles(ctx.session, ["administrator"]);
+
+        if (!authorized) {
+            throw new TRPCError({
+                code: "FORBIDDEN",
+                message: "Access denied to the requested resource",
+            });
+        }
+
+        await resend.emails.send({
+            from: "MyBPHS Ebéd <ebed@bphs.hu>",
+            to: [
+                "martin.reicher@budapest.school",
+                "mark.gyoni@budapest.school",
+            ],
+            subject: "Elérhető a jövő heti menü!",
+            react: Lunch(),
+        });
+    }),
+});
+
+export default emailRouter;
