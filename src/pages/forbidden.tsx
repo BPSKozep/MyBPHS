0d6e952d838e487ae359eeff3eaf96aa32da6e88
JamDon2 <hello@jamdon2.dev>
2024-03-31 12:39:40 +0200

    rework forbidden page

--- src/pages/forbidden.tsx
+++ src/pages/forbidden.tsx
@@ -9,24 +9,31 @@ function Forbidden() {
     const router = useRouter();
 
     return (
-        <div className="relative flex h-[93vh] w-full flex-col items-center justify-center gap-5 text-2xl font-bold text-white">
-            <span className="text-center">Hozzáférés megtagadva!</span>
+        <div className="flex h-full w-full flex-col items-center justify-center gap-4 text-white">
+            <span className="text-center text-2xl font-bold">
+                Hozzáférés megtagadva!
+            </span>
 
-            <div
-                className="flex h-14 w-14 items-center justify-center rounded-full bg-blue-500 transition-all hover:h-20 hover:w-20 hover:shadow-lg"
-                onClick={() => {
-                    setClicked(true);
-                    setTimeout(() => {
-                        router.push("/");
-                    }, 700);
-                }}
-            >
-                <motion.div
-                    animate={{ rotate: clicked ? 360 : 0 }}
-                    transition={{ duration: 0.5 }}
+            <div className="flex h-20 w-20 items-center justify-center">
+                <div
+                    className="flex h-14 w-14 items-center justify-center rounded-full bg-blue-500 transition-all hover:h-20 hover:w-20 hover:shadow-lg"
+                    onClick={() => {
+                        setClicked(true);
+                        setTimeout(() => {
+                            router.push("/");
+                        }, 700);
+                    }}
                 >
-                    <FontAwesomeIcon icon={faHome} className="transition-all" />
-                </motion.div>
+                    <motion.div
+                        animate={{ rotate: clicked ? 360 : 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        <FontAwesomeIcon
+                            icon={faHome}
+                            className="transition-all"
+                        />
+                    </motion.div>
+                </div>
             </div>
         </div>
     );
