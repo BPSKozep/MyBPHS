9c8016ff8281f97e918b62ff545b01bbe55026e7
markgyoni <markgyoni@gmail.com>
2024-04-01 21:05:45 +0200

    Enable lunch ordering button on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
                 <div className="m-3 inline-grid grid-cols-1 grid-rows-4 gap-4 text-white sm:grid-cols-2 sm:grid-rows-2">
                     <BigLinkButton title="Kreditek" url="/credits" disabled />
                     <BigLinkButton title="Órarend" url="/timetable" disabled />
-                    <BigLinkButton title="Ebédrendelés" url="/lunch" disabled />
+                    <BigLinkButton title="Ebédrendelés" url="/lunch" />
                     <BigLinkButton title="Coming Soon™" url="/" disabled />
                 </div>
             </div>
