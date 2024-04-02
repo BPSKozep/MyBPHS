7f03a6dba4857ff51f066e180ad8e606a7372d0e
JamDon2 <reichermartin4@gmail.com>
2024-04-02 13:18:11 +0200

    add admin button
    Co-authored-by: IoMARKoI <markgyoni@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import BigLinkButton from "components/BigLinkButton";
+import OnlyRoles from "components/OnlyRoles";
 
 export default function Home() {
     return (
         <>
             <div className="flex h-full w-full items-center justify-center">
                 <div className="m-3 inline-grid grid-cols-1 grid-rows-4 gap-4 text-white sm:grid-cols-2 sm:grid-rows-2">
+                    <BigLinkButton title="Ebédrendelés" url="/lunch" />
                     <BigLinkButton title="Kreditek" url="/credits" disabled />
                     <BigLinkButton title="Órarend" url="/timetable" disabled />
-                    <BigLinkButton title="Ebédrendelés" url="/lunch" />
-                    <BigLinkButton title="Coming Soon™" url="/" disabled />
+                    <OnlyRoles roles={["administrator"]}>
+                        <BigLinkButton title="Admin" url="/admin" />
+                    </OnlyRoles>
                 </div>
             </div>
         </>
