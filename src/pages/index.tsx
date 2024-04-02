a4faee019f855026a65b17b85fea7df291101370
JamDon2 <reichermartin4@gmail.com>
2024-04-02 18:39:55 +0200

    add title to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import BigLinkButton from "components/BigLinkButton";
 import OnlyRolesComponent from "components/OnlyRolesComponent";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>MyBPHS</title>
+            </Head>
             <div className="flex h-full w-full items-center justify-center">
                 <div className="m-3 inline-grid grid-cols-1 grid-rows-4 gap-4 text-white sm:grid-cols-2 sm:grid-rows-2">
                     <BigLinkButton title="Ebédrendelés" url="/lunch" />
