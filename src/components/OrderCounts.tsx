23bc1abeb259144885107d5d9a622a6f0648fb18
JamDon2 <reichermartin4@gmail.com>
2024-04-02 18:20:38 +0200

    improve ordercounts
    Co-authored-by: IoMARKoI <markgyoni@users.noreply.github.com>

--- src/components/OrderCounts.tsx
+++ src/components/OrderCounts.tsx
@@ -5,10 +5,10 @@ function OrderCounts({ data }: { data: Record<string, number> }) {
         <table>
             <thead>
                 <tr>
-                    <th className="text-md bg-gray-700 px-6 py-3 text-left font-medium tracking-wider text-white">
+                    <th className="bg-gray-700 px-6 py-3 text-left text-2xl font-bold tracking-wider text-white">
                         Opció
                     </th>
-                    <th className="text-md bg-gray-700 px-6 py-3 text-left font-medium tracking-wider text-white">
+                    <th className="bg-gray-700 px-6 py-3 text-left text-2xl font-bold tracking-wider text-white">
                         Mennyiség
                     </th>
                 </tr>
@@ -16,10 +16,10 @@ function OrderCounts({ data }: { data: Record<string, number> }) {
             <tbody>
                 {Object.entries(data).map((dataRow, index) => (
                     <tr key={index}>
-                        <td className="whitespace-nowrap bg-gray-700 px-6 py-3 text-left text-sm font-medium text-gray-100">
+                        <td className="whitespace-nowrap bg-gray-700 px-6 py-3 text-left text-xl font-medium text-gray-100">
                             {dataRow[0]}
                         </td>
-                        <td className="whitespace-nowrap bg-gray-700 px-6 py-3 text-left text-sm font-medium text-gray-100">
+                        <td className="whitespace-nowrap bg-gray-700 px-6 py-3 text-center text-xl font-medium text-gray-100">
                             {dataRow[1]}
                         </td>
                     </tr>
