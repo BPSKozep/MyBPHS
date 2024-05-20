ab99bf4931ac2d934962a9c4af5dcb37aee4d9f4
markgyoni <markgyoni@gmail.com>
2024-05-20 19:08:47 +0200

    Add NFCInput component from old lunch app to get name from token id

---
+++ src/components/NFCInput.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "utils/web-nfc.d.ts";
+
+function NFCInput({
+    nfc,
+    onChange,
+}: {
+    nfc: boolean;
+    onChange: (data: string) => void;
+}) {
+    const [data, setData] = useState("");
+
+    function onReading({ serialNumber }: NDEFReadingEvent) {
+        setData(serialNumber.replaceAll(":", ""));
+    }
+
+    useEffect(() => {
+        if (nfc && "NDEFReader" in window) {
+            const ndef = new NDEFReader();
+
+            ndef.addEventListener("reading", onReading);
+
+            ndef.scan();
+
+            return () => {
+                ndef.removeEventListener("reading", onReading);
+            };
+        }
+    }, [nfc]);
+
+    useEffect(() => {
+        onChange(data);
+    }, [data, onChange]);
+
+    return (
+        <>
+            <input
+                type="text"
+                value={data}
+                onChange={(e) => setData(e.target.value)}
+                className="rounded-md p-1 text-center"
+                placeholder="Token ID"
+            />
+        </>
+    );
+}
+
+export default NFCInput;
