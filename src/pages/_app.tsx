6cae9c8bfd6f7362ac8a8d7ee9b9a65ec04972dc
JamDon2 <reichermartin4@gmail.com>
2024-06-03 17:04:50 +0200

    remove redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,6 @@ import "@fontsource/lily-script-one";
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { trpc } from "utils/trpc";
-import { wrapper } from "store/store";
-import { Provider } from "react-redux";
 import { SessionProvider, useSession } from "next-auth/react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
@@ -74,12 +72,7 @@ function IdentifyUser({ children }: PropsWithChildren) {
     return children;
 }
 
-function App({ Component, ...rest }: AppProps) {
-    const { store, props } = wrapper.useWrappedStore(rest);
-    const {
-        pageProps: { session, ...pageProps },
-    } = props;
-
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     const router = useRouter();
 
     useEffect(() => {
@@ -94,29 +87,27 @@ function App({ Component, ...rest }: AppProps) {
 
     return (
         <PostHogProvider client={posthog}>
-            <Provider store={store}>
-                <SessionProvider session={session}>
-                    <OnlyAuthed enable={router.route !== "/forbidden"}>
-                        <IdentifyUser>
-                            <div className="box-border flex h-[100vh] w-full flex-col">
-                                <MainHeader />
-                                <AnimatePresence mode="wait">
-                                    <motion.div
-                                        key={router.route}
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        exit={{ opacity: 0 }}
-                                        transition={{ duration: 0.3 }}
-                                        className="h-full w-full"
-                                    >
-                                        <Component {...pageProps} />
-                                    </motion.div>
-                                </AnimatePresence>
-                            </div>
-                        </IdentifyUser>
-                    </OnlyAuthed>
-                </SessionProvider>
-            </Provider>
+            <SessionProvider session={session}>
+                <OnlyAuthed enable={router.route !== "/forbidden"}>
+                    <IdentifyUser>
+                        <div className="box-border flex h-[100vh] w-full flex-col">
+                            <MainHeader />
+                            <AnimatePresence mode="wait">
+                                <motion.div
+                                    key={router.route}
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                    className="h-full w-full"
+                                >
+                                    <Component {...pageProps} />
+                                </motion.div>
+                            </AnimatePresence>
+                        </div>
+                    </IdentifyUser>
+                </OnlyAuthed>
+            </SessionProvider>
         </PostHogProvider>
     );
 }
