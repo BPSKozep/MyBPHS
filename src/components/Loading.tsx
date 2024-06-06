dffcdc3f691c1d4d4bc57d437c9a12b93fd298b6
markgyoni <markgyoni@gmail.com>
2024-06-06 21:18:19 +0200

    Increase loading scale

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Loading() {
     return (
-        <div className="h-6 w-6 animate-spin rounded-full border-[0.22rem] border-solid border-gray-600 border-t-white" />
+        <div className="scale-150">
+            <div className="h-6 w-6 animate-spin rounded-full border-[0.22rem] border-solid border-gray-600 border-t-white" />
+        </div>
     );
 }
 
