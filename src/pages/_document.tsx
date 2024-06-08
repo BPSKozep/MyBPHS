a64de922090d8f972d9380f6da06e2b1641c2302
JamDon2 <reichermartin4@gmail.com>
2024-06-08 20:14:01 +0200

    remove nonexistent logos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,23 +16,6 @@ export default function Document() {
                 <meta name="mobile-web-app-capable" content="yes" />
                 <meta name="theme-color" content="#111827" />
 
-                <link rel="apple-touch-icon" href="/touch-icon-iphone.png" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="152x152"
-                    href="/touch-icon-ipad.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/touch-icon-iphone-retina.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="167x167"
-                    href="/touch-icon-ipad-retina.png"
-                />
-
                 <link
                     rel="icon"
                     type="image/png"
