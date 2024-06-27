9cd5ef53cdb00668c230e2bb97cfe38891789e53
markgyoni <markgyoni@gmail.com>
2024-06-27 11:47:39 +0200

    Changing pages to ssr part 2: admin/timetable [nodeploy]

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,9 +1,11 @@
-"use client";
-
 import CreateUsers from "components/CreateUsers";
 import OnlyRoles from "components/OnlyRoles";
 import PageWithHeader from "components/PageWithHeader";
 import React from "react";
+
+export const metadata = {
+    title: "MyBPHS - Admin / Felhasználók",
+};
 
 function Users() {
     return (
