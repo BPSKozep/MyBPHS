c55677391745c8d102b957f71b3fca2f3d4d7c97
JamDon2 <hello@jamdon2.dev>
2024-09-17 11:19:23 +0200

    fix house not appearing

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -14,7 +14,7 @@ function PageHeader({
         <header className="static flex h-16 flex-shrink-0 select-none items-center justify-center">
             <div className="flex cursor-default items-center text-white">
                 <Link href={homeLocation} className="absolute left-7">
-                    <FontAwesomeIcon icon={faHome} />
+                    <FontAwesomeIcon icon={faHome} className="h-5 w-5" />
                 </Link>
                 <h1 className="text-center text-xl font-black text-white sm:text-2xl">
                     {title}
