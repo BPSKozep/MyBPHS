38021ded5e9be8582400e1058ddc82088d303ddb
markgyoni <markgyoni@gmail.com>
2024-09-22 21:06:16 -0400

    Small loading animation for plus ordercounts

---
+++ src/components/SmallLoading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SmallLoading() {
+    return (
+        <div className="h-6 w-6 animate-spin rounded-full border-[0.2rem] border-solid border-gray-600 border-t-white" />
+    );
+}
+
+export default SmallLoading;
