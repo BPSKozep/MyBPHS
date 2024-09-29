fca73b0408fc9c10cd1d948e33348b6718c2aae4
JamDon2 <reichermartin4@gmail.com>
2024-09-29 14:27:32 +0200

    add new emails

---
+++ src/emails/password-reset.tsx
@@ -0,0 +1,58 @@
+import {
+    Body,
+    Container,
+    Hr,
+    Html,
+    Img,
+    Preview,
+    Section,
+    Text,
+    Tailwind,
+    CodeInline,
+} from "@react-email/components";
+
+import * as React from "react";
+
+function PasswordReset({ name, password }: { name: string; password: string }) {
+    return (
+        <Html>
+            <Preview>Elkészült az ideiglenes jelszavad.</Preview>
+            <Tailwind>
+                <Body className="bg-gray-900 font-sans">
+                    <Container className="mt-16 bg-slate-800 p-8">
+                        <Section className="">
+                            <Img
+                                src="https://cdn.bpskozep.hu/mybphs-logo.png"
+                                width="156"
+                                height="42"
+                            ></Img>
+                            <Hr />
+                            <Text className="text-lg font-bold leading-6 text-white">
+                                Kedves {name}!
+                            </Text>
+                            <Text className="text-lg leading-6 text-white">
+                                Ezzel az ideiglenes jelszóval tudsz belépni a
+                                laptopokra:{" "}
+                                <CodeInline className="rounded-md bg-gray-600 p-1">
+                                    {password}
+                                </CodeInline>
+                            </Text>
+
+                            <Hr />
+                            <Text className="text-lg leading-6 text-white">
+                                — A BPHS Rendszergazda csapata
+                            </Text>
+                            <Hr />
+                            <Text className="text-xs leading-4 text-slate-400">
+                                Budapest School Általános Iskola és Gimnázium,
+                                Budapest, II. János Pál pápa tér 25.
+                            </Text>
+                        </Section>
+                    </Container>
+                </Body>
+            </Tailwind>
+        </Html>
+    );
+}
+
+export default PasswordReset;
